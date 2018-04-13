eb68647032aec49d9d7b01b5b5c38b6531974c43
Stephen Bannasch <stephen.bannasch@gmail.com>
2018-04-13 02:19:39 -0400

    use query string to persist selection of model

--- models.js
+++ models.js
@@ -2,13 +2,23 @@
 window.onload=function() {
 
   var selectModel = document.getElementById("select-model");
-
   var model_frame = document.getElementById("model-iframe")
 
   function selectModelChange() {
-    model_frame.src = selectModel.value;
+    document.location.search = 'model=' + selectModel.value;
+    model_frame.src = selectModel.value + '/avalanche2d.html';
   };
 
   selectModel.onchange = selectModelChange;
+
+  let params = new URLSearchParams(document.location.search.substring(1));
+  var model = params.get('model');
+
+  if (model) {
+    selectModel.value = model;
+    model_frame.src = selectModel.value + '/avalanche2d.html';
+  } else {
+    document.location.search = 'model=' + selectModel.value;
+  }
 
 }
